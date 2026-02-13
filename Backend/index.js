a393e821b487161597626574d70454d60cf83caa
UjjwalTomar0808 <ujjwaltomar.0808@gmail.com>
2026-02-13 18:35:48 +0530

    made some changes

--- Backend/index.js
+++ Backend/index.js
@@ -2,12 +2,22 @@ import express from "express";
 import { ENV_CONFIG } from "./config/env-config.js";
 import { connectDB } from "./config/db-config.js";
 import { globalMiddleware as middleware } from "./middleware/index.js";
+import path from "path";
 
 const app = express();
 const PORT = ENV_CONFIG.PORT;
 
 // Apply middleware
 middleware(app);
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // Connect to database and start server
 const startServer = async () => {
--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8082/api/v1',
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+    withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
     },
